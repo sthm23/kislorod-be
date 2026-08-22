36274ffdf2ccc4ef330c73b4f2cc380703d41419
Sanjar Tukhtamishev <stuxtamixhev@gmail.com>
2026-08-22 06:39:04 +0500

    init project with prisma

---
+++ src/app.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common';
+import { OrdersModule } from './orders/orders.module';
+import { PrismaModule } from './prisma/prisma.module';
+
+@Module({
+  imports: [OrdersModule, PrismaModule],
+  controllers: [],
+  providers: [],
+})
+export class AppModule { }
